f51f382b20898c2436d2a9a59c71b58f384345cb
Miguel Roberts <miguelaroberts09@gmail.com>
2017-04-03 21:51:42 +0000

    Add Mongo
    Created job model aswell as added the basic functionality for the website. A persone can navigate to the add job page in the charity section and the job will appear in the "available jobs section for programmers.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var bodyParser = require("body-parser"),
-    express    = require("express"),
-    app        = express();
+var bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    express     = require("express"),
+    app         = express();
 
+// MONGOOSE CONFIG
+mongoose.connect("mongodb://localhost/charity_programmers");
+
+// JOB MODEL
+var Job = mongoose.model("Job", new mongoose.Schema({
+    title: String,
+    description: String,
+    charityName: String,
+    applicants: [String], 
+    status: {type: Number, default: 1}
+}));
+
+// APP CONFIG
 app.set("view engine", "ejs");
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,13 +24,46 @@ app.get("/", function(req, res){
     res.render("home", {cp: "n"}); 
 });
 
-app.get("/charity", function(req, res){
-   res.render("charity/charity", {cp: "c"}); 
+// ==============
+// CHARITY ROUTES
+// ==============
+
+app.get("/charity/job", function(req, res){
+   res.render("charity/index", {cp: "c", r: "index"}); 
 });
 
-app.get("/programmer", function(req, res){
-   res.render("programmer/programmer", {cp: "p"}); 
+app.get("/charity/job/new", function(req, res){
+   res.render("charity/new", {cp: "c", r: "new"}); 
 });
+
+app.post("/charity/job", function(req, res){
+    Job.create(req.body.job, function(err, newJob){
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/charity/job");
+        }
+    });
+});
+
+// =================
+// PROGRAMMER ROUTES
+// =================
+
+app.get("/programmer/job", function(req, res){
+   res.render("programmer/index", {cp: "p", r: "index"}); 
+});
+
+app.get("/programmer/job/all", function(req, res){
+    Job.find({}, function(err, allJobs){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("programmer/all", {cp: "p", r: "index", jobs: allJobs}); 
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("CharityProgrammers running...");
