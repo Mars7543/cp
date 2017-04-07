7449d07e9e69a2520113083db4bc85a80e2c0ac6
Miguel Roberts <miguelaroberts09@gmail.com>
2017-04-07 20:33:16 +0000

    Apply Finishing CSS Touches
    A user can register, log in, log out, and see nicely styled pages for each. Now we will add the functionality.

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ var mongoose = require("mongoose");
 
 // MONGOOSE CONFIG
 module.exports = {
-    url: "mongodb://localhost/charity_programmers"                                
+    url: "mongodb://Mars7543:password@ds155160.mlab.com:55160/charity_programmers"                                
 }
--- public/scripts/app.js
+++
@@ -1,5 +0,0 @@
-$(".container").css("margin-top", $("#main-menu").height() + 20);
-
-$('.message .close').on('click', function() {
-    $(this).closest('.message').transition('fade');
-});
