520b359b778d6bfe472e25b8cf179b334243fc22
Miguel Roberts <miguelaroberts09@gmail.com>
2017-04-20 17:07:15 +0000

    Update .gitignore

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@ var mongoose = require("mongoose");
 
 // mongo database
 module.exports = {
-    url: "mongodb://Mars7543:password@ds155160.mlab.com:55160/charity_programmers"                                
+    url: "mongodb://Mars7543:01245Abc@ds155160.mlab.com:55160/charity_programmers"                                
 }
